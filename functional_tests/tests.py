85d5b981988298547fa534ac7ea8abc812cd8f28
Mohammad <mohammad@pop-os.localdomain>
2019-05-24 18:11:20 +0200

    adding FT for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -97,3 +97,14 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('reading more about TDD in django', page_text)
         self.assertIn('buying milk', page_text)
+    
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(input_box.location['x'] + input_box.size['width']/2, 512, delta=10)
+        input_box.send_keys('testing')
+        input_box.send_keys(Keys.ENTER)
+        self.check_and_wait_for_row_in_list_table('1: testing')
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(input_box.location['x'] + input_box.size['width']/2 , 512, delta=10)
