1e45031b7dacb73338d9035cc2179415bf95a601
Mohammad <mohammad@pop-os.localdomain>
2019-06-02 01:20:03 +0200

    adding localhost to allowed hosts

--- testing_goat/settings.py
+++ testing_goat/settings.py
@@ -135,7 +135,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 if 'DJANGO_DEBUG_FALSE' in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-    ALLOWED_HOSTS = [os.environ['SITENAME']]
+    ALLOWED_HOSTS = [os.environ['SITENAME'], 'localhost', '127.0.0.1']
 
 else :
     DEBUG =True
